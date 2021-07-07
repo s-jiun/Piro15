4bc6b0e15a79488cf66d82f318a822b228b6955d
jiun <jiun48562@gmail.com>
2021-07-07 14:17:34 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -2,6 +2,7 @@ class OutOfRangeError(Exception):
     pass
 
 num = 0
+game_num = 0
 
 while True:
     try:
@@ -18,4 +19,24 @@ while True:
         break
 
 for i in range(num):
-    print("PlayerA : {0}".format(i+1))
+    game_num += 1
+    print("PlayerA : {0}".format(game_num))
+
+while True:
+    try:
+        num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능)) :"))
+        if num < 1 or num > 3:
+            raise OutOfRangeError
+    except ValueError:
+        print("정수를 입력하세요")
+        continue
+    except OutOfRangeError:
+        print("1,2,3 중 하나를 입력하세요")
+        continue
+    else:
+        break
+
+for i in range(num):
+    game_num += 1
+    print("PlayerB : {0}".format(game_num))
+
