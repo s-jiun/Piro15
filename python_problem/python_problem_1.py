69505caf90ac651b3d37e46bd1c687c4182e49fa
jiun <jiun48562@gmail.com>
2021-07-07 14:07:09 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,18 @@
+class OutOfRangeError(Exception):
+    pass
+
 num = 0
 
-num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능)) :"))
+while True:
+    try:
+        num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능)) :"))
+        if num < 1 or num > 3:
+            raise OutOfRangeError
+    except ValueError:
+        print("정수를 입력하세요")
+        continue
+    except OutOfRangeError:
+        print("1,2,3 중 하나를 입력하세요")
+        continue
+    else:
+        break
