9f7100915bc328361621322d5053048f977553b5
Jiun <jiun48562@gmail.com>
2021-07-16 01:23:55 +0900

    3-4강

--- AskdjangoBasic/askcompany/askcompany/urls.py
+++ AskdjangoBasic/askcompany/askcompany/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+def mysum(request, x, y):
+    result = x + y
+    return HttpResponse('result = {}'.format(result))
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('mysum/<int:x>/<int:y>/', mysum)
 ]
