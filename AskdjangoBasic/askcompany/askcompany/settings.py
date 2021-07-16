4e0d71c73b62941f84f9c307064935268dab43ab
Jiun <jiun48562@gmail.com>
2021-07-17 04:44:37 +0900

    16강

--- AskdjangoBasic/askcompany/askcompany/settings.py
+++ AskdjangoBasic/askcompany/askcompany/settings.py
@@ -38,11 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'debug_toolbar',
+    'django_extensions',
+
     'blog',
     'shop',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
--- AskdjangoBasic/askcompany/askcompany/urls.py
+++ AskdjangoBasic/askcompany/askcompany/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
@@ -26,4 +27,5 @@ urlpatterns = [
     path('mysum/<int:x>/<int:y>/', mysum),
     path('blog/', include('blog.urls')),
     path('shop/', include('shop.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
