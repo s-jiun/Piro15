040caac14aba519886dc7d6e7c6bee7aaf1fa034
Jiun <jiun48562@gmail.com>
2021-07-20 02:29:32 +0900

    (중급편) 7강

--- AskdjangoBasic/askdjango/accounts/views.py
+++ AskdjangoBasic/askdjango/accounts/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.shortcuts import render, redirect, resolve_url
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView
@@ -13,15 +14,26 @@ from .forms import SignupForm
 #         form = SignupForm(request.POST)
 #         if form.is_valid():
 #             user = form.save()
-#             return redirect(settings.LOGIN_URL)
+#             auth_login(request, user)
+#             return redirect('profile')
 #     else:
 #         form = SignupForm()
 #     return render(request, 'accounts/signup.html', {'form':form})
 
-signup = CreateView.as_view(model=User, 
-        form_class=SignupForm, 
-        template_name='accounts/signup.html',
-        success_url=settings.LOGIN_URL)
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = 'accounts/signup.html'
+
+    def get_success_url(self):
+        return resolve_url('profile')
+
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect(self.get_success_url())
+
+signup = SignupView.as_view()
 
 @login_required
 def profile(request):
