7d22f2b30b7e02e9384bcf97f47bbd2215161b9b
Jiun <jiun48562@gmail.com>
2021-07-19 19:19:13 +0900

    29강

--- AskdjangoBasic/askcompany/shop/urls.py
+++ AskdjangoBasic/askcompany/shop/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.item_list, name='item_list'),
     path('<int:pk>/', views.item_detail, name='item_detail'),
     # re_path(r'^(?P<pk>\d+)/$', views.item_detail),
+    path('new/', views.item_new, name='item_new'),
+    path('<int:pk>/edit/', views.item_edit, name='item_edit'),
 ]
