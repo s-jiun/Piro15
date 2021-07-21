f3fce6528c22ca8f78e58973dfc3c99d56b6e51e
Jiun <jiun48562@gmail.com>
2021-07-21 11:01:42 +0900

    add models

--- SWIDEA_SITE/ideas/models.py
+++ SWIDEA_SITE/ideas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from devtools.models import DevTool
 
 # Create your models here.
+
+class Idea(models.Model):
+    name = models.CharField(max_length=100, help_text="아이다어명을 입력하세요")
+    image = models.ImageField(blank=True, null=True, upload_to = 'idea_image/%Y/%m/%d/')
+    desc = models.TextField(help_text="아이디어에 대한 설명을 적어주세요")
+    likes = models.PositiveIntegerField(default=0)
